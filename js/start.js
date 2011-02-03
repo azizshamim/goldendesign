ef9ccd3becf62b9c597cb08ae02d670933dca60e
Aziz Shamim <azizshamim@gmail.com>
2011-02-03 00:44:45 -0600

    finally got the arcs working correctly- SCALE!

--- js/start.js
+++ js/start.js
@@ -14,36 +14,53 @@ function fibonacci(count) {
 // calculate the hypotenuse and return second point
 // eg (0,0) , 1 , 45
 //
-function arc(centerx, centery, area, theta) {
-  var radius = Math.sqrt(area)*100;
-  
+// return the hypotenuse and second point
+function vector(radius, theta) {
   var point2='';
   switch(theta)
   {
   case 45:
-    point2 = ""+radius+",-"+radius;
+    point2 = { x: radius, y: -radius };
     break;
   case 135:
-    point2 = "-"+radius+",-"+radius;
+    point2 = { x: -radius, y: -radius };
     break;
   case 225:
-    point2 = "-"+radius+","+radius;
+    point2 = { x: -radius, y: radius };
     break;
   case 315:
-    point2 = ""+radius+","+radius;
+    point2 = { x: radius, y: radius };
     break;
   default:
-    point2 = '';
+    point2 = {};
   }
-    
-  var svg = "M"+centerx+','+centery+" a"+radius+","+radius+" 0 0,0 "+point2;
-  return svg;
+  return point2;
 }
+
+
+function arc(start, area, theta) {
+  var radius = Math.sqrt(area)*10;
+  var end = vector(radius, theta);
+  return {origin: start, radius: radius, endpoint: end};
+}
+
+function svg(arc) {
+	return "M"+arc.origin.x+','+arc.origin.y+" a"+arc.radius+","+arc.radius+" 0 0,0 "+arc.endpoint.x+","+arc.endpoint.y;
+}
+
 
 $().ready(function() {
   var paper = Raphael('canvas',400,400);
-  var gold_arc = arc(200,200, 1, 45);
-  console.log(gold_arc);
-  var draw = paper.path(gold_arc);
+
+  var gold_arc = arc({ x:200,y:200}, 1, 45);
+  var svg1 = svg(gold_arc);
+  console.log(svg1);
+  var draw = paper.path(svg1);
   draw.attr("stroke", "#0f0");
+
+  var gold_arc2 = arc({x: gold_arc.origin.x+gold_arc.endpoint.x, y: gold_arc.origin.y+gold_arc.endpoint.y}, 2, 135);
+  svg2 = svg(gold_arc2);
+  console.log(svg2);
+  var draw2 = paper.path(svg2);
+  draw2.attr("stroke", "#00f");
 });
