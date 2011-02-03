0c22ebbdb3ab486ab73d6e970616b9745c3d41bd
Aziz Shamim <azizshamim@gmail.com>
2011-02-03 14:56:52 -0600

    full spiral working!

--- README.markdown
+++ README.markdown
@@ -3,16 +3,14 @@ Eventually a java scriptlet to let you draw an arc on a page
 Right now, a math problem to help me draw an arc on a blank page at origin x,y
 
 ##DONE
-
 1. Draw an arc
 2. Draw an arc of radius r
 3. Draw an arc of radius r when hypotenuse is given
 4. Draw an arc of radius r of hypotenuse h and vector theta
 5. Draw two arcs of radius r0 and r1 of hypotenuse h0 and h1 with ϑ0 and ϑ1 using the end point of arc n for origin of n+1
+6. Draw n arcs of hypotenuse h0 .. hn with the subsequent ϑ0 rotating 90degrees or π/2 
 
 ##TODO
-6. Draw n arcs of hypotenuse h0 .. hn with the subsequent ϑ0 rotating 90degrees or π/2 
 7. Draw n arcs rotating -π/2 or π/2  
 8. Add the squares (optional)
-8. Turn into bookmarklet
-
+9. Turn into bookmarklet
--- js/start.js
+++ js/start.js
@@ -1,20 +1,13 @@
-function fibonacci(count) {
-  return [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269 ];
-}
-
 // SVG templates for creating a quarter circle
-//
 // M(x,y)origin aradius,radius 0 0,0 -radius,-radius northeast-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 radius,radius   southwest-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 radius,-radius  southeast-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 -radius,radius  northwest-quartercircle
 
-// draw a quartercircle arc
-// given a box origin (x0,y0), area (a), and vector (v degrees) 
-// calculate the hypotenuse and return second point
-// eg (0,0) , 1 , 45
-//
-// return the hypotenuse and second point
+function fibonacci(count) {
+  return [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269 ];
+}
+
 function vector(radius, theta) {
   var point2='';
   switch(theta)
@@ -37,30 +30,38 @@ function vector(radius, theta) {
   return point2;
 }
 
+function svg(arc) {
+	return "M"+arc.origin.x+','+arc.origin.y+" A"+arc.radius+","+arc.radius+" 0 0,0 "+arc.endpoint.x+","+arc.endpoint.y;
+}
 
 function arc(start, area, theta) {
-  var radius = Math.sqrt(area)*10;
-  var end = vector(radius, theta);
-  return {origin: start, radius: radius, endpoint: end};
+  var radius = (Math.sqrt(2*area))
+  var v = vector(radius, theta);
+  var end = {x: start.x+v.x, y:start.y+v.y};
+  var graphic = svg({origin: start, radius: radius, endpoint: end });
+  return {svg: graphic, origin: start, endpoint: end};
 }
 
-function svg(arc) {
-	return "M"+arc.origin.x+','+arc.origin.y+" a"+arc.radius+","+arc.radius+" 0 0,0 "+arc.endpoint.x+","+arc.endpoint.y;
-}
+function draw(canvas, start, vectors, theta) {
+  // pop vector
+  v = vectors.shift();
+  if (v == undefined) {
+    return 0;
+  }
+  goldarc = arc(start, v, theta);
+  console.log(goldarc.svg);
+  var path = canvas.path(goldarc.svg);
+  path.attr("stroke", "#0f0");
 
+  theta1 = (theta+90)%360;
+  draw(canvas, goldarc.endpoint, vectors, theta1);
+}
 
 $().ready(function() {
-  var paper = Raphael('canvas',400,400);
+  var paper = Raphael('canvas',4000,4000);
+  draw(paper, {x:400,y:400}, fibonacci(10), 45);
+  
+  //$('#canvas').click(function(e) {
+  //});
 
-  var gold_arc = arc({ x:200,y:200}, 1, 45);
-  var svg1 = svg(gold_arc);
-  console.log(svg1);
-  var draw = paper.path(svg1);
-  draw.attr("stroke", "#0f0");
-
-  var gold_arc2 = arc({x: gold_arc.origin.x+gold_arc.endpoint.x, y: gold_arc.origin.y+gold_arc.endpoint.y}, 2, 135);
-  svg2 = svg(gold_arc2);
-  console.log(svg2);
-  var draw2 = paper.path(svg2);
-  draw2.attr("stroke", "#00f");
 });
