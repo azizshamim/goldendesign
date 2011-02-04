850b2f8637dccbfd9f05d941391a2605dd326fb2
Aziz Shamim <azizshamim@gmail.com>
2011-02-04 16:27:34 -0600

    click redraw done, fixed the math on the spiral and now it is golden

--- js/start.js
+++ js/start.js
@@ -1,67 +1,68 @@
+// Golden Spiral 
+// requires Raphael and jQuery
+//
 // SVG templates for creating a quarter circle
 // M(x,y)origin aradius,radius 0 0,0 -radius,-radius northeast-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 radius,radius   southwest-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 radius,-radius  southeast-quartercircle
 // M(x,y)origin aradius,radius 0 0,0 -radius,radius  northwest-quartercircle
-
-function fibonacci(count) {
-  return [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269 ];
-}
+//
 
 function vector(radius, theta) {
-  var point2='';
-  switch(theta)
-  {
-  case 45:
-    point2 = { x: radius, y: -radius };
-    break;
-  case 135:
-    point2 = { x: -radius, y: -radius };
-    break;
-  case 225:
-    point2 = { x: -radius, y: radius };
-    break;
-  case 315:
-    point2 = { x: radius, y: radius };
-    break;
-  default:
-    point2 = {};
-  }
+  var point2 = '';
+  switch (theta) {
+    case 45:
+      point2 = { x: radius, y: -radius };
+      break;
+    case 135:
+      point2 = { x: -radius, y: -radius };
+      break;
+    case 225:
+      point2 = { x: -radius, y: radius };
+      break;
+    case 315:
+      point2 = { x: radius, y: radius };
+      break;
+    default:
+      point2 = {};
+    }
   return point2;
 }
 
 function svg(arc) {
-	return "M"+arc.origin.x+','+arc.origin.y+" A"+arc.radius+","+arc.radius+" 0 0,0 "+arc.endpoint.x+","+arc.endpoint.y;
+  return "M"+arc.origin.x+','+arc.origin.y+" A"+arc.radius+","+arc.radius+" 0 0,0 "+arc.endpoint.x+","+arc.endpoint.y;
 }
 
-function arc(start, area, theta) {
-  var radius = (Math.sqrt(2*area))
+function arc(start, radius, theta) {
   var v = vector(radius, theta);
   var end = {x: start.x+v.x, y:start.y+v.y};
   var graphic = svg({origin: start, radius: radius, endpoint: end });
   return {svg: graphic, origin: start, endpoint: end};
 }
 
-function draw(canvas, start, vectors, theta) {
+function draw(canvas, start, vector, theta) {
   // pop vector
-  v = vectors.shift();
-  if (v == undefined) {
+  if (vector > 1000000) {
     return 0;
   }
-  goldarc = arc(start, v, theta);
+  var goldarc = arc(start, vector, theta);
   console.log(goldarc.svg);
   var path = canvas.path(goldarc.svg);
   path.attr("stroke", "#0f0");
+  path.attr('stroke-width', '5');
 
-  theta1 = (theta+90)%360;
-  draw(canvas, goldarc.endpoint, vectors, theta1);
+  vector = vector*1.618;
+  var theta1 = (theta+90)%360;
+  draw(canvas, goldarc.endpoint, vector, theta1);
 }
 
 $().ready(function() {
+  var angle = 315;
+  var start_vector = 1;
   var paper = Raphael('canvas',4000,4000);
-  draw(paper, {x:400,y:400}, fibonacci(10), 45);
   
-  //$('#canvas').click(function(e) {
-  //});
-
+  $('#canvas').click(function(e) {
+      paper.clear();
+      draw(paper, {x:e.pageX, y:e.pageY}, start_vector, angle);
+  });
 });
